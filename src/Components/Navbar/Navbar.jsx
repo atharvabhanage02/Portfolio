cb68d87bf6b6cbecc5e0e86f11ace9f9ce0fb01a
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-10-03 08:02:21 +0530

    style : navbar styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import "./navbar.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <a href="#">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#projects">
+        <AiOutlineHome />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export { Navbar };
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./projects.css";
 const Projects = () => {
-  return <div>Projects</div>;
+  return <section id="projects">Projects</section>;
 };
 
 export { Projects };
