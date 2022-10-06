b7890e3beae3888b57a116d3aa8ca29e16ceabf6
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-10-06 07:34:35 +0530

    style : About Section styled

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import "./about.css";
+import ABOUTIMG from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ABOUTIMG} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>Undergrad</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>XYZ</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>Done</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+            velit amet nemo corrupti, delectus culpa blanditiis eaque nostrum
+            sint fugit a temporibus officiis ab accusantium! A nostrum
+            blanditiis dolorem tenetur!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Lets Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export { About };
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <BiBook />
       </a>
-      <a href="#projects">
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
