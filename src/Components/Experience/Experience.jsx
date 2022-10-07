6fc1f393fc472d32d2a5a35934d8e4e25cb68355
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-10-07 21:43:18 +0530

    feat : Added Skills Section

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  const frontendSkills = [
+    "HTML",
+    "CSS",
+    "JS",
+    "React",
+    "Tailwind",
+    "TypeScript",
+    "Redux",
+  ];
+  const backendSkills = ['NodeJS','Express','Mongoose','Postman']
+  return (
+    <section id="experience">
+      <h5>Skills I Have</h5>
+      <h2>Experience</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {frontendSkills.map((skill) => {
+              return (
+                <article className="experience__details">
+                  <BsPatchCheckFill />
+                  <h4>{skill}</h4>
+                </article>
+              );
+            })}
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            {backendSkills.map((skill) => {
+              return (
+                <article className="experience__details">
+                  <BsPatchCheckFill />
+                  <h4>{skill}</h4>
+                </article>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export { Experience };
